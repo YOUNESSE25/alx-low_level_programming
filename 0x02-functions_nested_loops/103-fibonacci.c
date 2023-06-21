ea6cd2fa6b180ddf8228a72b402e34047f6ef347
HAOUAS Younesse <younessehaouas@gmail.com>
2023-06-21 16:44:54 -0700

    Functions, nested loops

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -6,21 +6,22 @@
  */
 int main(void)
 {
-        long f = 1;
-        long k = 1;
-        long l = 1;
-        long i;
+	long f = 1;
+	long k = 1;
+	long l = 1;
+	long i;
+	long s = 0;
 
-        for (i = 0; i < 4000000; i++)
-        {
-                l = k;
-                k = f;
-                f = k + l;
+	for (i = 0; i < 4000000; i++)
+	{
+		l = k;
+		k = f;
+		f = k + l;
 		if (f % 2 == 0)
 		{
 			s = s + f;
 		}
-        }
-        printf("%ld\n", s);
-        return (0);
+	}
+	printf("%ld\n", s);
+	return (0);
 }
