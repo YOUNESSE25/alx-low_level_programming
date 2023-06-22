9949540ab24c76593619bdc16182721e59cb638b
HAOUAS Younesse <younessehaouas@gmail.com>
2023-06-21 17:01:15 -0700

    Functions, nested loops

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ * main - Empty point
+ *
+ *Return: return 0
+ */
+int main(void)
+{
+	long f = 1;
+	long k = 1;
+	long l = 1;
+	long i;
+
+	for (i = 0; i < 98; i++)
+	{
+		printf("%ld, ", f);
+		l = k;
+		k = f;
+		f = k + l;
+	}
+	printf("%ld\n", f);
+	return (0);
+}
