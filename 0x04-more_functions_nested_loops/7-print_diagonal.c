3f486a11888a085eb30e591bb9dca191baafafdb
HAOUAS Younesse <younessehaouas@gmail.com>
2023-06-23 08:32:20 -0700

    More functions, more nested loops

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -16,9 +16,9 @@ void print_diagonal(int n)
 	}
 	else
 	{
-		for (i = 0; i < n; i++)
+		for (i = 1; i <= n; i++)
 		{
-			for (j = 0; j < n; j++)
+			for (j = 0; j < i; j++)
 			{
 				_putchar(32);
 			}
