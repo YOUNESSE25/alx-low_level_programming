9891f22aba0d2684014f68011752c5ab90a32726
HAOUAS Younesse <younessehaouas@gmail.com>
2023-06-22 19:37:18 -0700

    More functions, more nested loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,10 +14,7 @@ void more_numbers(void)
 		for (i = 0; j <= 14; j++)
 		{
 			if (j >= 10)
-			{
-				_putchar(49);
-			}
-			_putchar((j % 10) + 48);
+				_putchar('1');
 		}
 		_putchar('\n');
 	}
