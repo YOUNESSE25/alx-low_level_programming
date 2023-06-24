c7df22ccb593d49f95d6797bddfc336f08465249
HAOUAS Younesse <younessehaouas@gmail.com>
2023-06-23 19:39:23 -0700

    m

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,32 +6,22 @@
  */
 void print_number(int n)
 {
-	int a;
-	int w;
-	int m;
-	int l;
+	unsigned int a;
+	unsigned int l;
 
 	if (n < 0)
 	{
 		_putchar(45);
-		n = n * (-1);
+		a = n * (-1);
 	}
-	l = n / 1000;
-	m = (n % 1000) / 100;
-	a = (n % 100) / 10;
-	w = n % 10;
-	if (l > 0)
-		_putchar(l + 48);
-	if (m > 0)
-		_putchar(m + 48);
-	else if (m == 0 && l != 0)
-		_putchar(48);
-	if (a > 0)
-		_putchar(a + 48);
-	else if (a == 0 && (l + m) != 0)
-		_putchar(48);
-	if (w >= 0)
-		_putchar(w + 48);
 	else
-		_putchar(48);
+	{
+		a = n;
+	}
+	if (a / 10)
+	{
+		print_number(a / 10);
+	}
+	l = a % 10 +48;
+	_putchar(l);
 }
