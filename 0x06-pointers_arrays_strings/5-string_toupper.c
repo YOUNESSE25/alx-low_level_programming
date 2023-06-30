945e4e306398a651640686c96af799e230c73228
HAOUAS Younesse <younessehaouas@gmail.com>
2023-06-29 19:56:19 -0700

    p

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -13,10 +13,10 @@ char *string_toupper(char *n)
 
 	while (n[i] != '\0')
 	{
-		if (n[i] >= 97 && n[i] <= 122)
+		if (n[i] >= 'a' && n[i] <= 'z')
 		{
 			n[i] = n[i] - 32;
-		i++;
+			i++;
 		}
 	}
 	return (n);
