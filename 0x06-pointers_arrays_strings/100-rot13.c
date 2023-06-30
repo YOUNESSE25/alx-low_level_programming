c1dd6ff1713ca87c95a2f7736ec23394a3567ba9
HAOUAS Younesse <younessehaouas@gmail.com>
2023-06-30 13:13:58 -0700

    More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include"main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - encodes a string using rot13.
+ * @str: pointer
+ *
+ * Return: *s
+ */
+char *rot13(char *s)
+{
+	int i;
+	int j;
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char arot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = arot[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
