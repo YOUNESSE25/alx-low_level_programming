b130a4f09f355bd0da1c9a3663487df721fa2dc5
YOUNESSE25 <younessehaouas@gmail.com>
2023-07-12 17:24:34 -0700

    malloc

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,13 @@
+#include<stdlib.h>
+#include"main.h"
+/**
+ * strtow - splits a string into words.
+ * @str: char *
+ * Return: char **
+ */
+char **strtow(char *str)
+{
+	if (str ==  NULL || str == "")
+		return (NULL);
+}
+
