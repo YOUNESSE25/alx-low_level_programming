f4a94d4bfbc6e90641fdc0049d6d22ccf084161f
YOUNESSE25 <younessehaouas@gmail.com>
2023-07-13 16:56:58 -0700

    more malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,9 +15,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int size1 = 0;
 	unsigned int size2 = 0;
 
-	while (s1[size1])
+	while (s1 && s1[size1])
 		size1++;
-	while (s2[size2])
+	while (s2 && s2[size2])
 		size2++;
 	if (n >= size2)
 		t = malloc(sizeof(char) * (size1 + size2 + 1));
