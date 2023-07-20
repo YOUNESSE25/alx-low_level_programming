3a90c2378287e86cad0a42ce3f81c96e55cd33e2
YOUNESSE25 <younessehaouas@gmail.com>
2023-07-19 19:57:15 -0700

    test

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,54 @@
+#include "3-calc.h"
+
+/**
+ * op_add - sum
+ * @a: number1
+ * @b: number2
+ *
+ * Return: sum of number1 and 2
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - defference
+ * @a: number1
+ * @b: number2
+ * Return: difference of number1 and 2
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - product two numbres
+ * @a: number1
+ * @b: number2
+ *
+ * Return: product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - division two numbres
+ * @a: number1
+ * @b: number2
+ * Return: division
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - Returns modulo
+ * @a: number1
+ * @b: number2
+ * Return: modulo
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
