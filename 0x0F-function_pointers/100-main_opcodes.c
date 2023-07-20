53f4c23e4a98d5ad6e199510ba1b96cca638f249
YOUNESSE25 <younessehaouas@gmail.com>
2023-07-20 10:22:47 -0700

    Function pointers

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - prints the opcodes of its own main function
+ * @argc: num of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int b;
+	int i;
+	char *array;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	b = atoi(argv[1]);
+	if (b < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	array = (char *)main;
+	for (i = 0; i < b; i++)
+	{
+		printf("%02hhx ", array[i]);
+		if (i == b - 1)
+		{
+			printf("\n");
+			break;
+		}
+	}
+	return (0);
+}
