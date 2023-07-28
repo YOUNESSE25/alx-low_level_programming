e5a8bcfb7a60e0fcb2ff88ece8641c1f5acaf271
YOUNESSE25 <younessehaouas@gmail.com>
2023-07-27 17:40:38 -0700

    sad

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include<stdlib.h>
+#include"lists.h"
+/**
+ * free_list - frees linekd list
+ * @head: list to freed
+ */
+void free_list(list_t *head)
+{
+	list_t *t;
+
+	while (head)
+	{
+		t = head->next;
+		free(head->str);
+		free(head);
+		head = t;
+	}
+}
