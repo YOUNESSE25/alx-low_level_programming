df62816c866ca8cee0f417c5f5f59533335c51f6
YOUNESSE25 <younessehaouas@gmail.com>
2023-08-01 13:55:05 -0700

    More singly linked lists

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,7 +10,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int i;
 	listint_t *temp = head;
 
-	if (head == NULL)
+	if (temp == NULL)
 		return (NULL);
 	for (i = 0; i < index; i++)
 	{
