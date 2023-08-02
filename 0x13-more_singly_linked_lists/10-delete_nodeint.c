15f0fdb983a89dcda7aa8199134a2641237d08d9
YOUNESSE25 <younessehaouas@gmail.com>
2023-08-01 18:09:48 -0700

    More singly linked lists

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -27,6 +27,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		if (temp->next == NULL)
 			return (-1);
 		temp = temp->next;
+		i++;
 	}
 	new = temp->next;
 	temp->next = new->next;
