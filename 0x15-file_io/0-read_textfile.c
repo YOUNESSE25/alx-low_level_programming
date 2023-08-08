5fff0cc76f92cbb26ab288c4494361c5fed212d3
YOUNESSE25 <younessehaouas@gmail.com>
2023-08-08 05:08:36 -0700

    File I/O

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,8 @@
 #include<stdlib.h>
 #include"main.h"
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output.
+ * read_textfile - reads a text file and prints it
+ * to the POSIX standard output.
  * @filename: pointer file
  * @letters: num of letters
  * Return: the actual number of letters it could read and print or 0
@@ -13,6 +14,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t wwrite;
 	char *buf;
 
+	if (filename == NULL)
+		return (0);
 	buf = malloc(sizeof(char) * letters);
 	if (buf == NULL)
 		return (0);
