964572aac0da06b07e441af4ab36c924c2469eff
YOUNESSE25 <younessehaouas@gmail.com>
2023-09-14 16:12:32 -0700

    test

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include "lists.h"
+/**
+ * print_dlistint - print dlistint list elements
+ * @h: pointr debut list
+ * Return: int or node
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t a;
+	for (a = 0; h != NULL; a++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (a);
+}
